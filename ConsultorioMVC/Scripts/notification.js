3224c4dbfeb9e3d0510def0f2bf71265a450d693
MartinSola55 <martinrsola55@gmail.com>
2023-10-24 16:53:58 -0300

    Changed notification

--- ConsultorioMVC/Scripts/notification.js
+++ ConsultorioMVC/Scripts/notification.js
@@ -1,14 +1,4 @@
 ﻿$(document).ready(function () {
-    if ($("#txtNotification").html() !== "") {
-        setTimeout(function () {
-            $("#notificationContainer").addClass("hidden");
-        }, 8000)
-        setTimeout(function () {
-            $("#notificationContainer").addClass("d-none");
-        }, 10000)
-    } else {
-        $("#notificationContainer").addClass("d-none");
-    }
     $("#btnCloseNotification").on("click", function () {
         $("#notificationContainer").addClass("hidden");
         setTimeout(function () {
